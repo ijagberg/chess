485d8de5784a9216d8575b63c8a14c5af1f24c4c
Isak Jägberg <ijagberg@gmail.com>
2021-05-02 17:28:54 +0200

    Add Game to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,6 @@
+pub use crate::board::Board;
 pub use crate::consts::*;
+pub use crate::game::Game;
 pub use crate::piece::Piece;
 pub use crate::piece::PieceType;
-pub use crate::Board;
 pub use crate::Color;
